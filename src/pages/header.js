ac48a7f526c38afb9bb73a39ae8a026dcb7dd8ae
bonkegcobo <madixmicro@gmail.com>
2022-11-03 19:02:15 +0200

    Create the mobile menu

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import menu from '../image/ham.svg'
+import image from '../image/xmark-solid.svg'
 import './styles/header.css'
+
 
 const Header = () => {
     let link = {
-       textDecoration:'none',
-      };
+        textDecoration: 'none',
+    };
     const [showMobMenu, setMobMenu] = useState(false)
 
     const menuBarClicked = () => {
-       
+
         setMobMenu(!showMobMenu);
     }
 
     return (
-        <header className="header-container">
-            <h1>
-                Bonke
-            </h1>
-            <FontAwesomeIcon icon={faBars} onClick={() => menuBarClicked()} />
-            <div className={showMobMenu?"mob-menu":"show"}>
-              <FontAwesomeIcon icon={faXmark} style={{width:'100%'}} />
+        <>
+            <header className="header-container">
+                <h1 className="title">
+                    Bonke
+                </h1>
+                <img src={menu} onClick={()=>menuBarClicked()} alt="menuBar"/>
+
+            </header>
+            <div className={showMobMenu ? "show" : "mob-menu"}>
+            
                 <ul className="links">
-                    <li className="link"><NavLink style={link} to="/">Home</NavLink></li>
-                    <li className="link"><NavLink style={link} to="Projects">Projects</NavLink></li>
-                    <li className="link"><NavLink style={link} to="Contact">Contact</NavLink></li>
+                    <li className="link"><NavLink style={link} onClick={()=>menuBarClicked()} to="/">Home</NavLink></li>
+                    <li className="link"><NavLink style={link} onClick={()=>menuBarClicked()} to="Projects">Projects</NavLink></li>
+                    <li className="link"><NavLink style={link} onClick={()=>menuBarClicked()} to="Contact">Contact</NavLink></li>
                 </ul>
+                <div className="close-Tag">
+                    <img className="cancel_btn" src={image} alt="closing button" onClick={()=>menuBarClicked()} />
+                </div>
             </div>
-        </header>
+        </>
     )
 }
 export default Header;
